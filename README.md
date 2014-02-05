0decf93363056fbf75092721321b815ccecd7846
0x51DB <ChristopherWalker2016@unorthwestern.edu>
2014-02-04 18:51:38 -0600

    added scraper.py
    a few commands to remove extraneous JSON data

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ========
 
 Playing around with NLP ... for my EECS 337 class
+
+scraper.py : JSON library operations were running slowly on my dual core, so I decided to blame the unnecessary JSON data like date and username, as well as structural things. scraper.py removes this from goldenglobes.json to create scraped.lines, consisting of text only on every line, with no extra data.
---
+++ scraper.py
@@ -0,0 +1,19 @@
+fr = open('goldenglobes.json','r')
+fw = open('scraped.lines','w')
+read_data = fr.read()
+i = 0
+while (i < len(read_data)) :
+    testval = read_data.find("\"text\" : \"", i)
+    if testval != -1 :
+        i = testval + 10
+        checkboundval = 1
+        boundval = i
+        while(checkboundval == 1) :
+            boundval = read_data.find("\"", boundval + 1)
+            if (read_data[boundval - 1] != '\\') :
+                checkboundval = 0
+        fw.write(read_data[i:boundval])
+        fw.write("\n")
+        i = boundval + 1
+fr.close()
+fw.close()
